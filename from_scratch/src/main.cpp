ea4cd13a8642b06ca5de8dbf1f7e0ddcee790636
Wu Ruiwen <2946996247@qq.com>
2024-04-24 10:14:33 +0800

    实现基本type_traits

--- from_scratch/src/main.cpp
+++ from_scratch/src/main.cpp
@@ -1,13 +1,21 @@
 
 #include<iostream>
 #include"./impl/÷«ƒ‹÷∏’Î/unique_ptr.h"
+#include"./impl/type_traits/type_traits.h"
 
 int main() {
-	
-	Entity* e = new Entity(12);
+	//using namespace wrw;
+
+	//≤‚ ‘type_traits.h
+	//cout << wrw::is_int<int>::value << endl;
+	//cout << wrw::is_int<float>::value << endl;
+	//cout << wrw::is_int<const int>::value << endl;
+	//cout << wrw::is_int_helper<const int>::value << endl;
+
+	/*Entity* e = new Entity(12);
 	wrw::unique_ptr<Entity> up(e);
 	cout << "e = " << e << endl;
-	cout << "up = " << up << endl;
+	cout << "up = " << up << endl;*/
 
-	//¥˝≤‚ ‘
+	
 }
