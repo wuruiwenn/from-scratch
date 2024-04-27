9f49eab04d447db06af1ff134f368b3586b37d56
Wu Ruiwen <2946996247@qq.com>
2024-04-27 21:16:56 +0800

    单例模式

--- from_scratch/src/main.cpp
+++ from_scratch/src/main.cpp
@@ -16,56 +16,11 @@ std::ostream& operator<<(std::ostream& out, const User& u) {
 	out << "[user:" << u.id << "]\n";
 	return out;
 }
+#include"./µ•¿˝ƒ£ Ω/single.h"
 int main() {
 
-	//Entity* e = new Entity(12);
-	////Entity* e2 = new Entity(24);
-	//wrw::unique_ptr<Entity> p(e);
-	//p.reset();
-	//using namespace wrw;
-	//wrw::unique_ptr<Entity> up;
-	//wrw::unique_ptr<Entity> up1();
-	//wrw::unique_ptr<Entity> up2(new Entity(9));
-
-	//≤‚ ‘type_traits.h
-	//cout << wrw::is_int<int>::value << endl;
-	//cout << wrw::is_int<float>::value << endl;
-	//cout << wrw::is_int<const int>::value << endl;
-	//cout << wrw::is_int_helper<const int>::value << endl;
-
-	
-	//wrw::unique_ptr<Entity> up(e);
-	//cout << "e = " << e << endl;
-	//cout << "up = " << up << endl;
-	/*Entity* e = new Entity(12);
-	Entity* e2 = new Entity(24);
-
-	wrw::unique_ptr<Entity> up1(e);
-	up1.reset();
-	wrw::unique_ptr<Entity> up2(e2);
-	cout << "*e = " << *e;
-	cout << "*up1 = " << *up1;
-	cout << "*up2 = " << *up2;
-	cout << "*e2 = " << *e2;
-	up1.swap(up2);
-	cout << "swap....\n";
-	cout << "*e = " << *e;
-	cout << "*up1 = " << *up1;
-	cout << "*up2 = " << *up2;
-	cout << "*e2 = " << *e2;*/
-	/*wrw::unique_ptr<Entity> up(e);
-	up.reset();*/
-
-	//cout << "*e = " << *e;
-	////cout << "*e1 = " << *e1;
-	//cout << "*up = " << *up;
-	//std::unique_ptr<Entity> up(e);
-	//up.reset();
-	//cout << "*up = " << *up;
-	Entity* e = new Entity(2);
-	cout << "*e = " << *e;
-	std::unique_ptr<Entity> up(e);
-	up.reset();
-	cout << "*e = " << *e;
-	cout << "*up="<<*up;
+	Single* s1 = Single::GetIns();
+	Single* s2 = Single::GetIns();
+	cout << s1 << endl;//00007FF65CF6E160
+	cout << s2 << endl;//00007FF65CF6E160
 }
