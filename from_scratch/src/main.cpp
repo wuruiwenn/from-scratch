096423b6b01ee6609f5ed6b843d2fa08460ac3e9
Wu Ruiwen <2946996247@qq.com>
2024-04-29 20:21:02 +0800

    实现String类，及其拷贝，移动构造、=运算符

--- from_scratch/src/main.cpp
+++ from_scratch/src/main.cpp
@@ -4,6 +4,7 @@
 #include"./智能指针/unique_ptr.h"
 #include"./单例模式/single.h"
 #include"./数据结构/single_list.h"
+#include"./数据结构/string.h"
 
 class User {
 	int id;
@@ -21,7 +22,7 @@ std::ostream& operator<<(std::ostream& out, const User& u) {
 
 int main() {
 	using namespace wrw;
-	SingleList* s = new SingleList();
+	/*SingleList* s = new SingleList();
 	for (int i = 1; i <= 5; i++)
 	{
 		s->addLast(i);
@@ -29,6 +30,48 @@ int main() {
 	cout << *s;
 	cout << "reverse...\n";
 	s->reverse();
-	cout << *s;
+	cout << *s;*/
+	String s("abcdef");
+	String s1(s);//执行之前，s1此时并没有被构造出来，正是通过s来构造s1的
 
+	String s2("DEG");
+	String s3("XYZ");
+	s2 = s3;
+
+	cout << s;
+	cout << s1;
+	cout << s2;
+	cout << s3;
+
+	cout << "...\n";
+	String s5("ABCDEF");
+	String s6(std::move(s5));
+
+	if (s5) {
+		cout << "s5 = " << s5;
+	}else {
+		cout << "s5 NULL\n";
+	}
+	if (s6) {
+		cout << "s6 = " << s6;
+	}else {
+		cout << "s6 NULL\n";
+	}
+	cout << ".........\n";
+	String s7("ABCDEF");
+	String s8("abcdef");
+	s7 = std::move(s8);
+	if (s7) {
+		cout << "s7 = " << s7;
+	}
+	else {
+		cout << "S7 NULL\n";
+	}
+
+	if (s8) {
+		cout << "s8 = " << s8;
+	}
+	else {
+		cout << "s8 NULL\n";
+	}
 }
---
+++ from_scratch/src/数据结构/string.h
@@ -0,0 +1,104 @@
+#pragma once
+
+//实现类似 Java String
+
+#include<string>
+namespace wrw
+{
+	class String {
+	private:
+		char* m_buffer;
+		int m_size;
+	public:
+		~String() {
+			delete[] m_buffer;
+			cout << "String 析构函数\n";
+		}
+		String():m_buffer(nullptr), m_size(0) {}
+
+		String(const char* str) {
+			int n = strlen(str);
+			m_size = n;
+			m_buffer = new char[n+1];
+			for (int i = 0; i < n; i++) {
+				m_buffer[i] = str[i];
+			}
+			m_buffer[m_size] = '\0';
+			cout << "String 构造函数\n";
+		}
+		//拷贝构造函数
+		//拷贝构造调用的时机：当前对象，即this，此时并没有被构造
+		//正是通过传进来的目标对象进行构造
+		String(const String& s) {
+			int n = s.m_size;
+			this->m_buffer = new char[n+1];
+			this->m_size = n;
+			for (int i = 0; i < n; i++) {
+				m_buffer[i] = s.m_buffer[i];
+			}
+			m_buffer[n] = '\0';
+			cout << "String 拷贝构造函数\n";
+		}
+
+		//拷贝 = 赋值运算符
+		// = 赋值运算符调用时机：当前对象，即this已经被初始化了，
+		// 等待传入的目标对象覆盖自己
+		String& operator=(const String& s){
+			int n = s.m_size;
+			this->m_size = n;
+			//this->m_buffer = new char[n + 1];//此时当前对象是已经初始化了的，不必重新初始化
+			//只需覆盖
+			for (int i = 0; i < n; i++) {
+				m_buffer[i] = s.m_buffer[i];
+			}
+			m_buffer[n] = '\0';
+			cout << "String = 赋值运算符\n";
+			return *this;
+		}
+
+		//移动构造函数
+		//把目标对象s的内容转移到this
+		//移动构造 调用时机：当前对象this此时还未被构造，且传入的目标对象是 右值
+		String(String&& s) {
+			int n = s.m_size;
+			this->m_size = n;
+			this->m_buffer = new char[n + 1];
+			for (int i = 0; i < n; i++) {//目标对象的内容移动到this
+				this->m_buffer[i] = s.m_buffer[i];
+			}
+			this->m_buffer[n] = '\0';
+			s.m_buffer = nullptr;//目标对象"置空"
+			s.m_size = 0;
+
+			cout << "String 移动构造\n";
+		}
+
+		//移动 = 赋值运算符
+		String& operator=(String&& s) {
+			int n = s.m_size;
+			this->m_size = n;
+			//this->m_buffer = new char[n + 1];
+			for (int i = 0; i < n; i++) {
+				this->m_buffer[i] = s.m_buffer[i];
+			}
+			this->m_buffer[n] = '\0';
+			s.m_buffer = nullptr;//目标对象"置空"
+			s.m_size = 0;
+			cout << "String = 移动赋值运算符\n";
+			return *this;
+		}
+
+		//使得String对象可隐式转换为一个bool值
+		operator bool() {
+			return m_buffer != nullptr;
+		}
+		int GetStrSize() {
+			return m_size;
+		}
+		friend std::ostream& operator<<(std::ostream& out, const String& s);
+	};
+	std::ostream& operator<<(std::ostream& out, const String& s) {
+		out << "[String:" << s.m_buffer << "]\n";
+		return out;
+	}
+}
