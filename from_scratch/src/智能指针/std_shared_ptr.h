5e5c039659a47e2e9b3b34034ae378a9694fffab
Wu Ruiwen <2946996247@qq.com>
2024-05-14 22:30:36 +0800

    std::shared_ptr增加Counter计数器类

--- from_scratch/src/智能指针/std_shared_ptr.h
+++ from_scratch/src/智能指针/std_shared_ptr.h
@@ -41,11 +41,77 @@ namespace wrw2
 		}
 
 		/*
+			拷贝构造：
+				拷贝other的目标对象
+				拷贝other的引用计数Counter
+				最重要的是：更新目标对象的shared_ptr的引用计数+1
+		*/
+		shared_ptr(const shared_ptr& other) {
+			obj = other.obj;
+			counter = other.counter;
+			++(counter->share_cnt);
+			cout << "shared_ptr 拷贝构造\n";
+		}
+
+		/*
+			拷贝 = 赋值运算符
+		*/
+		shared_ptr& operator=(const shared_ptr& other) {
+			if (this == &other || obj == other.obj) {
+				return *this;
+			}
+			release();
+			obj = other.obj;
+			counter = other.counter;
+			++(counter->share_cnt);
+
+			return *this;
+			cout << "shared_ptr 拷贝 = 赋值运算符\n";
+		}
+
+		/*
+			移动构造
+		*/
+		shared_ptr(shared_ptr&& other) {
+
+			obj = other.obj;
+			counter = other.counter;
+			++(counter->share_cnt);
+			other.release();
+
+			cout << "shared_ptr 移动构造\n";
+		}
+
+		/*
+			移动 = 赋值运算符
+		*/
+		shared_ptr& operator=(shared_ptr&& other) {
+			if (this == &other) {
+				return *this;
+			}
+			if (obj == other.obj)
+			{
+				other.release();
+			}
+			else
+			{
+				release();
+				obj = other.obj;
+				counter = other.counter;
+				++(counter->share_cnt);
+				other.release();
+			}
+			return *this;
+			cout << "shared_ptr 移动 = 赋值运算符\n";
+		}
+
+		/*
 			release():
 			解除当前shared_ptr对目标对象的控制权，
 			解除绑定，意味着 引用计数--，所以：
-				若shared_ptr引用计数 = 0，则销毁目标对象
-				若weak_ptr引用计数 = 0，则Counter计数器也需销毁
+				若shared_ptr引用计数 = 0，则销毁目标对象 obj
+				若shared_ptr引用计数=0 && weak_ptr引用计数 = 0,
+					则 Counter计数器也需销毁
 		*/
 		void release()
 		{
@@ -53,13 +119,17 @@ namespace wrw2
 			if (0 == (counter->share_cnt))
 			{
 				delete obj;
-				obj = nullptr;
-				if (0 == (counter->share_cnt))
+				//obj = nullptr;
+				if (0 == (counter->weak_cnt))//这里不是很明白，为什么weak_cnt=0，要delete cnter？
 				{
 					delete counter;
-					counter = nullptr;
+					//counter = nullptr;
 				}
 			}
+			//我觉得这里，无论如何，
+			//不管目标对象有没有被delete，也应该断开绑定
+			obj = nullptr;
+			counter = nullptr;
 		}
 
 		/*
@@ -80,8 +150,7 @@ namespace wrw2
 			}
 		}
 
-
-
+		//一些成员函数
 		T* get() const {
 			return obj;
 		}
