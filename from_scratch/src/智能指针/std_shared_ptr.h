b02267a1b1227766921857a612ef5f55d4700484
Wu Ruiwen <2946996247@qq.com>
2024-05-15 22:50:41 +0800

    增加weak_ptr

--- from_scratch/src/智能指针/std_shared_ptr.h
+++ from_scratch/src/智能指针/std_shared_ptr.h
@@ -179,4 +179,103 @@ namespace wrw2
 		int share_cnt = 0;
 		int weak_cnt = 0;
 	};
+
+
+	template<class T>
+	class weak_ptr {
+	public:
+		friend class shared_ptr<T>;
+	private:
+		T* obj;
+		Counter* counter;
+	public:
+		/*
+			weak_ptr构造函数：
+			weak_ptr只能通过shared_ptr来构造，不能直接传入目标对象obj
+		*/
+		weak_ptr() :obj(nullptr), counter(nullptr) {}
+		weak_ptr(shared_ptr<T>& sp)
+			:obj(sp.obj), counter(sp.counter)
+		{
+			if (counter) {
+				++(counter->weak_cnt);
+			}
+		}
+
+		//析构
+		~weak_ptr() {
+			release();
+		}
+
+		//拷贝构造
+		weak_ptr(const weak_ptr& other) {
+			obj = other.obj;
+			counter = other.counter;
+			++(counter->weak_cnt);
+			cout << "weak_ptr 拷贝构造\n";
+		}
+
+		/*
+			拷贝 = 赋值运算符
+			有2种：
+				传入weak_ptr
+				传入shared_ptr
+		*/
+		weak_ptr& operator=(const weak_ptr& other) {
+			if (this == &other) {
+				return *this;
+			}
+			release();
+			obj = other.obj;
+			counter = other.counter;
+			++(counter->weak_cnt);
+			return *this;
+		}
+		weak_ptr& operator=(const shared_ptr& other) {
+			//传入shared_ptr，自然不可能是同一个对象
+			release();
+			obj = other.obj;
+			counter = other.counter;
+			++(counter->weak_cnt);
+			return *this;
+		}
+
+
+
+		/*
+			重要函数：
+				release()
+				lock()
+				expired()
+		*/
+		void release()
+		{
+			if (!counter)
+				return;
+
+			--(counter->weak_cnt);
+			if (0 == (counter->weak_cnt) && 0 == (counter->share_cnt))
+			{
+				delete counter;
+			}
+			counter = nullptr;
+		}
+
+		/*
+			lock()：
+			从weak_ptr中获取一个shared_ptr对象，
+			若目标对象已经被销毁，则返回空的shared_ptr，否则返回完整的shared_ptr
+		*/
+		shared_ptr<T>& lock() {
+			return shared_ptr<T>(*this);
+		}
+
+		/*
+			expired():
+				判断目标对象是否仍存活
+		*/
+		bool expired() {
+
+		}
+	};
 }
