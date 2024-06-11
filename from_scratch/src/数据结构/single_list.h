9517358040350a90ba295e55a312bcdbe85c3138
Wu Ruiwen <2946996247@qq.com>
2024-06-11 20:08:21 +0800

    单链表v2

--- from_scratch/src/数据结构/single_list.h
+++ from_scratch/src/数据结构/single_list.h
@@ -96,3 +96,64 @@ namespace wrw
 		return out;
 	}
 }
+
+/*也可以这样实现，用head，tail2个指针表示单链表的头和尾*/
+
+/*
+template<class T>
+struct Node {
+	T val;
+	Node* next;
+	Node(const T& value)
+		:val(value), next(nullptr)
+	{}
+};
+
+template<class T>
+class MyList {
+private:
+	Node<T>* head;
+	Node<T>* tail;
+	int size;
+public:
+	MyList()
+		:head(nullptr),tail(nullptr),size(0)
+	{}
+	void push_back(const T& val) {
+		if (head == nullptr) {
+			head = new Node(val);
+			tail = head;
+		}
+		else {
+			tail->next = new Node(val);
+			tail = tail->next;
+		}
+		size++;
+	}
+	template<class T>
+	friend std::ostream& operator<<(std::ostream& out, const MyList<T>& l);
+};
+template<class T>
+std::ostream& operator<<(std::ostream& out, const MyList<T>& l) {
+	if (l.size == 0) {
+		out << "[]\n";
+		return out;
+	}
+	Node<T>* ptr = l.head;
+	out << "[";
+	while (ptr != l.tail) {
+		out << ptr->val << "->";
+		ptr = ptr->next;
+	}
+	out << l.tail->val << "]\n";
+	return out;
+}
+int main() {
+	MyList<int> list;
+	for (int k = 1; k <= 5; k++) {
+		list.push_back(k);
+	}
+	cout << list;
+}
+
+*/
