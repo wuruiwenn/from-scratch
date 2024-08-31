801863d1c49a4b3b63c4cd9e42e1e2cba2260550
Wu Ruiwen <2946996247@qq.com>
2024-08-31 16:36:10 +0800

    :apple:增加std::string详细实现，带迭代器

--- from_scratch/src/数据结构/string_with_iterator.h
+++ from_scratch/src/数据结构/string_with_iterator.h
@@ -5,7 +5,6 @@
 //带迭代器的 std::string
 //是STL中 std::string、iterator的一个简单实现
 
-
 namespace wrw {
 	class string
 	{
@@ -14,7 +13,7 @@ namespace wrw {
 		int m_size;
 		int m_capacity;
 	public:
-		//���캯��
+		//构造函数
 		/*string()
 			:m_ptr(new char[1]), m_size(0), m_capacity(0) {
 			m_ptr[0] = '\0';
@@ -27,7 +26,7 @@ namespace wrw {
 			strcpy(m_ptr, str);
 		}*/
 
-		//�ϲ�Ϊ��Ĭ�ϲ����Ĺ��캯��
+		//合并为带默认参数的构造函数
 		string(const char* str = "")
 			: m_size(strlen(str)) {
 			m_capacity = m_size;
@@ -35,19 +34,19 @@ namespace wrw {
 			strcpy(m_ptr, str);
 		}
 
-		//����
+		//析构
 		~string() {
 			delete[] m_ptr;
 			m_size = m_capacity = 0;
 		}
 
-		//��������
+		//拷贝构造
 		string(const string& other) {
 			m_size = other.m_size;
 			m_capacity = m_size;
-			m_ptr = new char[m_capacity + 1];//Ĭ�Ͽ���������ǳ�������������ʵ�����������double delete
+			m_ptr = new char[m_capacity + 1];//默认拷贝构造是浅拷贝，这里必须实现深拷贝，否则double delete
 			strcpy(m_ptr, other.m_ptr);
-			cout << "string��������\n";
+			cout << "string拷贝构造\n";
 		}
 		string& operator=(const string& other) {
 			if (this == &other) {
@@ -58,7 +57,7 @@ namespace wrw {
 			m_capacity = m_size;
 			m_ptr = new char[m_capacity + 1];
 			strcpy(m_ptr, other.m_ptr);
-			cout << "string���� = ��ֵ�����\n";
+			cout << "string拷贝 = 赋值运算符\n";
 			return *this;
 		}
 
@@ -67,11 +66,11 @@ namespace wrw {
 			return m_ptr;
 		}
 
-		char& operator[](int pos) {//�����ⲿͨ��[]���޸�string�����ڵ��ַ�
+		char& operator[](int pos) {//允许外部通过[]来修改string对象内的字符
 			_ASSERT(pos < m_size);
 			return m_ptr[pos];
 		}
-		const char& operator[](int pos) const {//�ײ�Ҳ�ɼ���const����ֹ�ⲿ�޸ĸ��ַ�
+		const char& operator[](int pos) const {//首部也可加上const，禁止外部修改该字符
 			_ASSERT(pos < m_size);
 			return m_ptr[pos];
 		}
@@ -82,22 +81,22 @@ namespace wrw {
 
 		void Print(const string& str) {
 			for (int i = 0; i < m_size; i++) {
-				cout << str[i] << " ";//����������϶�������ͨ��[]�޸�string����ģ�����operator[]������const
+				cout << str[i] << " ";//这里遍历，肯定不允许通过[]修改string对象的，所以operator[]必须是const
 			}
 			cout << endl;
 		}
 	public:
-		//ʵ�� string ������
-		//����Ϊ �ڲ���
+		//实现 string 迭代器
+		//定义为 内部类
 		class string_iterator {
 		private:
 			char* obj;
 		public:
 			string_iterator(char* ptr = nullptr) :obj(ptr) {}
 
-			//�������
+			//正向迭代
 			string_iterator& operator++() {//++i
-				++obj;//��������ָ��һ��char��λ��ָ�룬ֱ��++��ÿ��++��Ծһ��char����
+				++obj;//本身就是指向一个char单位的指针，直接++，每次++跳跃一个char宽度
 				return *this;
 			}
 			string_iterator operator++(int) {//i++
@@ -120,7 +119,7 @@ namespace wrw {
 			}
 		};
 	public:
-		//���� ������ ���ͱ���
+		//定义 迭代器 类型别名
 		typedef string_iterator iterator;
 		iterator begin() {
 			return iterator(m_ptr);
@@ -136,37 +135,36 @@ namespace wrw {
 		return out;
 	}
 }
-//int main() {
-//	using wrw::string;
-//	string s("ABCDEFG");
-//
-//	cout << "����������string\n";
-//	for (auto it = s.begin(); it != s.end(); it++) {
-//		cout << (*it) << " ";
-//	}
-//	cout << endl;
-//	for (auto cur : s) {//���ʾ��ǵ��õ�������Ŀ�����ʵ���˵���������ʹ��
-//		cout << cur << " ";
-//	}
-//	cout << endl;
-//
-//	//����������
-//	auto it = s.begin();
-//	cout << "it init = " << *it << endl;
-//	//++it;
-//	//++it;
-//	//cout << "*it = " << *it << endl;
-//	auto it2 = it++;
-//	cout << "*it2 = " << *it2 << endl;
-//	cout << "*it = " << *it << endl;
-//
-//	cout << "bool equals\n";
-//	auto it3 = s.begin();
-//	it3++;
-//	it3++;
-//	auto it4 = s.begin();
-//	it4++;
-//	++it4;
-//	cout << "it3 == it4��" << (it3 == it4) << endl;
-//
-//}
+int main() {
+	using wrw::string;
+	string s("ABCDEFG");
+
+	cout << "迭代器访问string\n";
+	for (auto it = s.begin(); it != s.end(); it++) {
+		cout << (*it) << " ";
+	}
+	cout << endl;
+	for (auto cur : s) {//本质就是调用迭代器，目标对象实现了迭代器就能使用
+		cout << cur << " ";
+	}
+	cout << endl;
+
+	//迭代器操作
+	auto it = s.begin();
+	cout << "it init = " << *it << endl;
+	//++it;
+	//++it;
+	//cout << "*it = " << *it << endl;
+	auto it2 = it++;
+	cout << "*it2 = " << *it2 << endl;
+	cout << "*it = " << *it << endl;
+
+	cout << "bool equals\n";
+	auto it3 = s.begin();
+	it3++;
+	it3++;
+	auto it4 = s.begin();
+	it4++;
+	++it4;
+	cout << "it3 == it4：" << (it3 == it4) << endl;
+}
